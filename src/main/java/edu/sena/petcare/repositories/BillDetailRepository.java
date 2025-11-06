639eb3c9437ae0355e99c35ed5dda7f24243a5b6
lauraguerrero01 <laura.j.guerrero.r@gmail.com0>
2025-11-06 15:34:38 -0500

    Eliminación de bill

--- src/main/java/edu/sena/petcare/repositories/BillDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sena.petcare.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.sena.petcare.models.BillDetail;
-
-public interface BillDetailRepository extends JpaRepository<BillDetail, Long> {
-} {
-    
-}
--- src/main/java/edu/sena/petcare/repositories/BillTaxesRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.sena.petcare.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.sena.petcare.models.BillTaxes;
-
-public interface BillTaxesRepository extends JpaRepository<BillTaxes, Long> {
-}
